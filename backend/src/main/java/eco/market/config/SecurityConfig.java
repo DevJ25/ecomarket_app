f301b4313a08f997ad3d841a2e3a4dbccceea55c
DevJ25 <alavrezsanti@gmail.com>
2025-12-12 08:42:48 -0500

    cross resueltosss

--- backend/src/main/java/eco/market/config/SecurityConfig.java
+++ backend/src/main/java/eco/market/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
                 Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://localhost:5174",
-                        "https://ecomarketbuyer.onrender.com", "https://ecomarketseller.onrender.com"));
+                        "https://ecomarket-app.onrender.com/", "https://ecomarketseller.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
--- frontendBuyer/src/config.ts
+++ frontendBuyer/src/config.ts
@@ -1,1 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+export const API_URL = BASE_URL.endsWith('/api') ? BASE_URL : `${BASE_URL}/api`;
